72bc2205720a8a3a8d28232ec049cc6ffbeee9db
kn3ny <30493783+kn3ny@users.noreply.github.com>
2021-05-30 13:17:32 +0900

    Add box

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import cbstyle from '../styles/ColorBox.module.scss'
 
 export default function Home() {
   return (
@@ -13,6 +14,10 @@ export default function Home() {
         <h1 className={styles.title}>
           slight
         </h1>
+
+        <div className={cbstyle.blue} />
+        <div className={cbstyle.spacer} />
+        <div className={cbstyle.blue} />
       </main>
     </div>
   )
